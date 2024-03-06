2c2f43af73cfb148ab6af673009852728e274ed4
rhyanndev <rhyannjosebezerra@gmail.com>
2024-03-06 20:36:15 -0300

    encaminhando arquivos do teste

---
+++ teste-1/target-test.js
@@ -0,0 +1,15 @@
+
+let indice = 13;
+let soma = 0;
+let k = 0;
+
+while(k < indice){
+
+    k += 1;
+
+    soma += k;
+}
+
+document.getElementById('result').textContent = soma;
+
+console.log(soma);
